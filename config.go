06176be03ccce1c1f2aef0120bb22c009ca59668
mahldcat <mahldcat@gmail.com>
2022-06-23 22:24:30 -0700

    update to add parsing functions

--- config.go
+++ config.go
@@ -1,17 +1,49 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
 )
 
-func GetConfigEntry(key string) (string, error) {
+type Configuration struct {
+	Connection string
+}
+
+//GetConfiguration will read in configuraiton data based off a file "configuration.json"
+func GetConfiguration() (Configuration, error) {
+	return getConfigImpl("configuration.json")
+}
+
+//GetConfiguration will read in configuration data based off the supplied path argument
+func GetConfigurationNamedFile(path string) (Configuration, error) {
+	return getConfigImpl(path)
+}
+
+func getConfigImpl(path string) (Configuration, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+
+	if err != nil {
+		return Configuration{}, err
+	}
+
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err = decoder.Decode(&configuration)
+
+	return configuration, err
+}
+
+//GetConfigEntry will return an entry
+//TODO: Research generic structs?
+func GetConfigEntry(conf *Configuration, key string) (string, error) {
 	if key == "" {
 		return "", errors.New("empty string")
 	}
 
-	config := fmt.Sprintf("Fetch config entry value for Key:= %v", key)
+	c := fmt.Sprintf("Key[%v]:=", key)
 
-	return config, nil
-
+	return c, nil
 }
